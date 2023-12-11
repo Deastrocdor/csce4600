37eec2ad0f624c8c24ef5dfca526ed31a95431d4
Deastrocdor <143763381+Deastrocdor@users.noreply.github.com>
2023-12-10 20:22:07 -0600

    Update mkdir_test.go

---
+++ builtins/mkdir_test.go
@@ -0,0 +1,39 @@
+package builtins
+
+import (
+    "os"
+    "path/filepath"  
+    "testing"
+)
+
+func TestMakeDirectory(t *testing.T) {
+    tmpDir := t.TempDir() // temp directory
+
+    // Define test cases
+    tests := []struct {
+        name    string
+        dirPath string
+        mode    os.FileMode
+        wantErr bool
+    }{
+        {"CreateDir", "testdir", 0755, false},
+        {"CreateNestedDir", "nested/dir", 0755, false},
+        {"CreateDirNoPermission", "/invalid/dir", 0755, true},
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            fullPath := filepath.Join(tmpDir, tt.dirPath)
+            err := MakeDirectory(fullPath, tt.mode)  // remove builtins
+            if (err != nil) != tt.wantErr {
+                t.Errorf("MakeDirectory() error = %v, wantErr %v", err, tt.wantErr)
+            }
+
+            if !tt.wantErr {
+                if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+                    t.Errorf("MakeDirectory() did not create directory")
+                }
+            }
+        })
+    }
+}
